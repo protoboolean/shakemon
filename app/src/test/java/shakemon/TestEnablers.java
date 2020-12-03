67ab927ac5cc875d887a97f614222950472cc990
Basilikode <basilikode@pm.me>
2020-12-03 21:16:05 +0100

    Adopt EnabledIfEnvironmentVariable

--- app/src/test/java/shakemon/TestEnablers.java
+++
@@ -1,7 +0,0 @@
-package shakemon;
-
-public class TestEnablers {
-    public static boolean testPokeAPI() {
-        return System.getenv("SHAKEMON_POKEAPI_TESTING") != null;
-    }
-}
--- app/src/test/java/shakemon/pokemon/PokeAPITest.java
+++ app/src/test/java/shakemon/pokemon/PokeAPITest.java
@@ -1,13 +1,13 @@
 package shakemon.pokemon;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.EnabledIf;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 import shakemon.Config;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@EnabledIf(value = "shakemon.TestEnablers#testPokeAPI", disabledReason = "Test Depends on External Service. Used for " +
-        "development only ATM.")
+@EnabledIfEnvironmentVariable(named = "SHAKEMON_ALLOW_NETWORK_IO_TEST", matches = "true",
+        disabledReason = "Test Depends on External Service. Used for development only ATM.")
 class PokeAPITest {
     Config config = Config.load();
     PokeAPI pokeAPI = new PokeAPI(config.pokeApiUrl());
